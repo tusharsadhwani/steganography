11cc811bb70482d6d79997cf872532a4a5190787
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2019-08-07 17:09:21 +0530

    Initial commit

---
+++ decode.py
@@ -0,0 +1,70 @@
+import argparse
+import os.path
+import sys
+from PIL import Image
+
+def decode_image(imgpath):
+    img_name = os.path.basename(imgpath).split('.')[0]
+
+    out = ""
+    byte = ""
+    try:
+        with Image.open(imgpath) as img:
+            pixels = img.load()
+            for i in range(img.size[0]):
+                for j in range(img.size[1]):
+                    rgb_list = list(pixels[i, j])
+
+                    blue = rgb_list[-1]
+                    if blue % 2:
+                        byte += "1"
+                    else:
+                        byte += "0"
+                    
+                    if len(byte) == 8:
+                        if byte == "00000000":
+                            break
+                        # print(byte)
+                        out += byte
+                        byte = ""
+                if byte == "00000000":
+                    break
+            
+            
+            output_text = ""
+            for i in range(0, len(out), 8):
+                byte = out[i:i+8]
+                output_text += binary_to_char(byte)
+
+            outfile_name = f"decoded-{img_name}.txt"
+            with open(outfile_name, 'w') as outfile:
+                outfile.write(output_text)
+            
+            print(outfile_name, "created")
+            
+
+    except OSError:
+            msg = "File '{}' is not a valid image"
+            raise TypeError(msg.format(imgpath))
+
+
+def binary_to_char(binary):
+    asc = int(binary, base=2)
+    if asc >= 128:
+        print(binary)
+        raise ValueError("Unknown Input Characters.")
+    return chr(asc)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image", help="path to image to be encoded")
+    
+    args = parser.parse_args()
+    if args.image:
+        if os.path.exists(args.image):
+            decode_image(args.image)
+        else:
+            msg = "{} doesn't exist"
+            raise ValueError(msg.format(args.image))
+    else:
+        parser.print_help(sys.stderr)
---
+++ encode.py
@@ -0,0 +1,75 @@
+import argparse
+import os.path
+import sys
+from PIL import Image
+
+def encode_image(imgpath, text):
+    binary_text = ""
+    for char in text:
+        # print(char_to_binary(char))
+        binary_text += char_to_binary(char)
+    binary_text += "00000000"
+
+    img_name = os.path.basename(imgpath).split('.')[0]
+    text_index = 0
+    try:
+        with Image.open(imgpath) as img:
+            pixels = img.load()
+
+            MAX_BINARY_LENGTH = img.size[0] * img.size[1]
+            MAX_BINARY_LENGTH -= MAX_BINARY_LENGTH % 8
+
+            if len(binary_text) > MAX_BINARY_LENGTH:
+                print("WARNING: Text too large, truncating...")
+                print("TIP: Use larger resolution image to encode the entire text.")
+            binary_text = binary_text[:MAX_BINARY_LENGTH]
+
+            for i in range(img.size[0]):
+                for j in range(img.size[1]):
+                    rgb_list = list(pixels[i, j])
+                    blue = rgb_list[-1]
+                    if blue % 2:
+                        if binary_text[text_index] == '0':
+                            blue -= 1
+                            rgb_list[-1] = blue
+                            pixels[i, j] = tuple(rgb_list)
+                    else:
+                        if binary_text[text_index] == '1':
+                            blue += 1
+                            rgb_list[-1] = blue
+                            pixels[i, j] = tuple(rgb_list)
+
+                    text_index += 1
+                    if text_index == len(binary_text): break
+                if text_index == len(binary_text): break
+            
+            img.save(f"encoded-{img_name}.png")
+            print("Done!")
+
+    except OSError:
+            msg = "File '{}' is not a valid image"
+            raise TypeError(msg.format(imgpath))
+
+
+def char_to_binary(char):
+    if ord(char) >= 128:
+        raise ValueError("Unknown Input Characters.")
+    return "{:0>8s}".format(bin(ord(char))[2:])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image", help="path to image to be encoded")
+    parser.add_argument("--text", help="text to be encoded in")
+    
+    args = parser.parse_args()
+    if args.image and args.text:
+        if os.path.exists(args.image) and os.path.exists(args.text):
+            with open(args.text, 'r') as textfile:
+                text = textfile.read()
+            
+            encode_image(args.image, text)
+        else:
+            msg = "{} doesn't exist"
+            raise ValueError(msg.format(args.image))
+    else:
+        parser.print_help(sys.stderr)
